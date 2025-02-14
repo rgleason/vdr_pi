ef125db389217cde20e26dbeb37cad748a283833
Rasbats <miker@members.swis.net>
2025-02-14 12:43:35 +0000

    Android.12

--- src/vdr_pi_control.cpp
+++ src/vdr_pi_control.cpp
@@ -232,6 +232,9 @@ VDRControl::VDRControl(wxWindow* parent, wxWindowID id, vdr_pi* vdr)
       m_pvdr(vdr),
       m_isDragging(false),
       m_wasPlayingBeforeDrag(false) {
+
+  m_window = this;
+
   wxColour cl;
   GetGlobalColor(_T("DILG1"), &cl);
   SetBackgroundColour(cl);
