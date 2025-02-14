d4f7d7aeb89a9e4559a496ee4348ab2f60ef5824
Rasbats <miker@members.swis.net>
2025-02-14 15:47:28 +0000

    Android.19

--- src/vdr_pi_control.cpp
+++ src/vdr_pi_control.cpp
@@ -56,9 +56,9 @@ END_EVENT_TABLE()
 bool m_binResize = false;
 wxWindow* g_Window = m_window;
 
-Bind(wxEVT_LEFT_DOWN, &VDRControl::OnMouseEvent, g_Window);
-Bind(wxEVT_LEFT_UP, &VDRControl::OnMouseEvent, g_Window);
-Bind(wxEVT_MOTION, &VDRControl::OnMouseEvent, g_Window);
+VDRControl::Bind(wxEVT_LEFT_DOWN, &VDRControl::OnMouseEvent, g_Window);
+VDRControl::Bind(wxEVT_LEFT_UP, &VDRControl::OnMouseEvent, g_Window);
+VDRControl::Bind(wxEVT_MOTION, &VDRControl::OnMouseEvent, g_Window);
 
 #endif
 
